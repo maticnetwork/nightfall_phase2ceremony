9dd08945054f85f17bb47668e84421bac561fb69
David <druiz0992@gmail.com>
2022-04-29 09:46:42 +0200

    fix: compute b2sum of input and output

--- mpc/mpc_contribute.sh
+++ mpc/mpc_contribute.sh
@@ -2,6 +2,7 @@
 
 echo "What is your name?"
 read NAME
+NAME=$(echo "${NAME}" | tr -d '[:space:]')
 
 echo "NO ONE EXPECTS THE... Entropy source I'm asking you:"
 read ENTROPY
@@ -13,17 +14,39 @@ if ! docker --version; then
     sh get-docker.sh
 fi
 
-rm -rf ./params/out
+rm -rf ./params
+mkdir -p ./params
+mkdir -p ./params/out
+
+echo "Downloading param files..."
+curl https://nightfallv3-proving-files.s3.eu-west-1.amazonaws.com/phase2/mpc_params/deposit --output params/deposit 2> /dev/null
+echo "Deposit param file downloaded..."
+curl https://nightfallv3-proving-files.s3.eu-west-1.amazonaws.com/phase2/mpc_params/single_transfer --output params/single_transfer 2> /dev/null
+echo "Single transfer param file downloaded..."
+curl https://nightfallv3-proving-files.s3.eu-west-1.amazonaws.com/phase2/mpc_params/double_transfer --output params/double_transfer 2> /dev/null
+echo "Double transfer param file downloaded..."
+curl https://nightfallv3-proving-files.s3.eu-west-1.amazonaws.com/phase2/mpc_params/withdraw --output params/withdraw 2> /dev/null
+echo "Withdraw param file downloaded..."
+
 docker build -t contribute --build-arg NAME="${NAME}" --build-arg ENTROPY="${ENTROPY}" .
 docker run --name contribution_container contribute
 docker cp contribution_container:/app/params/out ./params
 docker rm -f contribution_container
 
-cd params/out 
+sleep 10
+cd params 
+b2sum deposit > out/in_deposit.b2sum
+b2sum double_transfer > out/in_double_transfer.b2sum
+b2sum single_transfer > out/in_single_transfer.b2sum
+b2sum withdraw > out/in_withdraw.b2sum
+cd out
 b2sum deposit > deposit.b2sum
 b2sum double_transfer > double_transfer.b2sum
 b2sum single_transfer > single_transfer.b2sum
 b2sum withdraw > withdraw.b2sum
-cd -
-tar -cvzf nightfall.phase2.contrib.${NAME}.tgz params/out
-echo "Thank you so much for your contribution to Polygon Nightfall phase2! Please send back the param file nightfall.phase2.contrib.${NAME}.tgz" 
+cd ../../
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+contribution=nightfall.phase2.contrib."${NAME}".${current_time}.tgz
+tar -cvzf ${contribution} params/out
+
+echo "Thank you so much for your contribution to Polygon Nightfall phase2! Please send back the param file ${contribution}" 
