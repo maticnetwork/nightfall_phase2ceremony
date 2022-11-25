b3188d0cd697168a1a313c7a8744db9c7d7c317f
José Pedro Sousa <github@zepedro.me>
2022-11-25 15:10:29 +0000

    feature: adding beacon functionality

--- mpc/beacon.js
+++ mpc/beacon.js
@@ -13,67 +13,83 @@ const Promise = require('promise');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const branchName = require('current-git-branch');
-const circuits = ['deposit', 'burn', 'tokenise', 'transfer', 'withdraw'];
+let circuits = ['deposit'];
 
-program.name('beacon-util').description('CLI').version('0.8.0');
+program.name('beacon').description('CLI').version('0.8.0');
 
-program.description('Beacon').action(async () => {
-  const name = await promptly.prompt('Name: ');
-  const beaconHash = await promptly.prompt('Beacon entropy: ');
-  const branch = await promptly.prompt('Git branch (optional): ', { default: branchName() });
-  console.log('Using branch: ', branch);
+program
+  .description('Beacon')
+  .argument('[branch]', 'The branch to use in your contribution')
+  .argument('[beaconHash]', 'The beacon hash to apply')
+  .argument('[circuit]', 'Apply beacon to a specific circuit only')
+  .action(async (branch, beaconHash, circuit) => {
+    if (!beaconHash) beaconHash = await promptly.prompt('Beacon hash: ');
+    if (!branch) branch = branchName();
 
-  const promises = [];
-  circuits.map(async circuit => {
-    promises.push(
-      new Promise(async (resolve, reject) => {
-        s3.listObjects({ Bucket: `mpc2`, Prefix: `${branch}/${circuit}` }, (err, data) => {
-          if (err) throw err;
-          const contributions = data.Contents.filter(cont => cont.Key !== `${circuit}/`).sort(
-            (a, b) => new Date(b.LastModified) - new Date(a.LastModified),
-          );
-          s3.getObject({ Bucket: `mpc2`, Key: `${contributions[0].Key}` }, async (err, data) => {
-            // await zKey.contribute(data.Body, `contribution_${circuit}.zkey`, name, entropy);
-            await zKey.beacon(data.Body, `beacon_${circuit}.zkey`, 'beacon', beaconHash, 10);
+    if (circuit && circuits.find(el => el === circuit)) circuits = [circuit];
+    console.log('Using branch: ', branch);
+    console.log('Applying hash', beaconHash);
+    console.log('Contributing to circuits:', circuits);
 
-            const formData = new FormData();
-            const testPath = path.join(__dirname, `beacon_${circuit}.zkey`);
-            formData.append('contribution', fs.createReadStream(testPath));
-            formData.append('name', name);
-            formData.append('circuit', circuit);
+    const promises = [];
+    circuits.map(async circuit => {
+      promises.push(
+        new Promise(async (resolve, reject) => {
+          s3.listObjects({ Bucket: `mpc2`, Prefix: `${branch}/${circuit}` }, (err, data) => {
+            if (err) throw err;
+            const contributions = data.Contents.filter(cont => cont.Key !== `${circuit}/`).sort(
+              (a, b) => new Date(b.LastModified) - new Date(a.LastModified),
+            );
+            s3.getObject({ Bucket: `mpc2`, Key: `${contributions[0].Key}` }, async (err, data) => {
+              // await zKey.contribute(data.Body, `contribution_${circuit}.zkey`, name, entropy);
+              const res = await zKey.beacon(
+                data.Body,
+                `beacon_${circuit}.zkey`,
+                'beacon',
+                beaconHash,
+                10,
+              );
 
-            var config = {
-              method: 'post',
-              url: `http://${
-                branch !== 'main' ? `${branch}.ceremony` : 'ceremony'
-              }.polygon-nightfall.io:3333/upload`,
-              headers: {
-                ...formData.getHeaders(),
-              },
-              data: formData,
-            };
+              if (!res) throw Error('Invalid beacon hash');
 
-            axios(config)
-              .then(function (response) {
-                console.log(chalk.green('Applied beacon to circuit ' + circuit));
-                console.log(chalk.blue('Verification:'));
-                console.log(chalk.blue(response.data.verification));
-                resolve();
-              })
-              .catch(function (error) {
-                console.log(chalk.red(error));
-                reject();
-              });
+              const formData = new FormData();
+              const testPath = path.join(__dirname, `beacon_${circuit}.zkey`);
+              formData.append('contribution', fs.createReadStream(testPath));
+              formData.append('name', 'beacon');
+              formData.append('circuit', circuit);
+
+              var config = {
+                method: 'post',
+                url: `http://${
+                  branch !== 'main' ? `${branch}.ceremony` : 'ceremony'
+                }.polygon-nightfall.io:3333/upload`,
+                headers: {
+                  ...formData.getHeaders(),
+                },
+                data: formData,
+              };
+
+              axios(config)
+                .then(function (response) {
+                  console.log(chalk.green('Applied beacon to circuit ' + circuit));
+                  console.log(chalk.blue('Verification:'));
+                  console.log(chalk.blue(response.data.verification));
+                  resolve();
+                })
+                .catch(function (error) {
+                  console.log(chalk.red(error));
+                  reject();
+                });
+            });
           });
-        });
-      }),
-    );
-  });
+        }),
+      );
+    });
 
-  await Promise.all(promises).then(() => {
-    console.log(chalk.bgGreen('Thank you for your contribution!'));
-    process.exit(0);
+    await Promise.all(promises).then(() => {
+      console.log(chalk.bgGreen('Thank you for your contribution!'));
+      process.exit(0);
+    });
   });
-});
 
 program.parse();
--- mpc/contribute.js
+++ mpc/contribute.js
@@ -13,66 +13,77 @@ const Promise = require('promise');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const branchName = require('current-git-branch');
-const circuits = ['deposit', 'burn', 'tokenise', 'transfer', 'withdraw'];
+let circuits = ['deposit', 'burn', 'tokenise', 'transfer', 'withdraw'];
 
 program.name('contribute-util').description('CLI').version('0.8.0');
 
-program.description('Contribute').action(async () => {
-  const name = await promptly.prompt('Name: ');
-  const entropy = await promptly.prompt('Entropy: ');
-  const branch = await promptly.prompt('Git branch (optional): ', { default: branchName() });
-  console.log('Using branch: ', branch);
+program
+  .description('Contribute')
+  .argument('[branch]', 'The branch to use for contribution')
+  .argument('[name]', 'Your name')
+  .argument('[entropy]', 'The entropy for this contribution')
+  .argument('[circuit]', 'Contribute to a specific circuit only')
+  .action(async (branch, name, entropy, circuit) => {
+    if (!name) name = await promptly.prompt('Your name: ');
+    if (!entropy) entropy = await promptly.prompt('Some entropy please:');
+    if (!branch) branch = branchName();
 
-  const promises = [];
-  circuits.map(async circuit => {
-    promises.push(
-      new Promise(async (resolve, reject) => {
-        s3.listObjects({ Bucket: `mpc2`, Prefix: `${branch}/${circuit}` }, (err, data) => {
-          if (err) throw err;
-          const contributions = data.Contents.filter(cont => cont.Key !== `${circuit}/`).sort(
-            (a, b) => new Date(b.LastModified) - new Date(a.LastModified),
-          );
-          s3.getObject({ Bucket: `mpc2`, Key: `${contributions[0].Key}` }, async (err, data) => {
-            await zKey.contribute(data.Body, `contribution_${circuit}.zkey`, name, entropy);
+    if (circuit && circuits.find(el => el === circuit)) circuits = [circuit];
+    console.log('Name:', name);
+    console.log('Entropy', entropy);
+    console.log('Using branch: ', branch);
+    console.log('Contributing to circuits:', circuits);
 
-            const formData = new FormData();
-            const testPath = path.join(__dirname, `contribution_${circuit}.zkey`);
-            formData.append('contribution', fs.createReadStream(testPath));
-            formData.append('name', name);
-            formData.append('circuit', circuit);
+    const promises = [];
+    circuits.map(async circuit => {
+      promises.push(
+        new Promise(async (resolve, reject) => {
+          s3.listObjects({ Bucket: `mpc2`, Prefix: `${branch}/${circuit}` }, (err, data) => {
+            if (err) throw err;
+            const contributions = data.Contents.filter(cont => cont.Key !== `${circuit}/`).sort(
+              (a, b) => new Date(b.LastModified) - new Date(a.LastModified),
+            );
+            s3.getObject({ Bucket: `mpc2`, Key: `${contributions[0].Key}` }, async (err, data) => {
+              await zKey.contribute(data.Body, `contribution_${circuit}.zkey`, name, entropy);
 
-            var config = {
-              method: 'post',
-              url: `http://${
-                branch !== 'main' ? `${branch}.ceremony` : 'ceremony'
-              }.polygon-nightfall.io:3333/upload`,
-              headers: {
-                ...formData.getHeaders(),
-              },
-              data: formData,
-            };
+              const formData = new FormData();
+              const testPath = path.join(__dirname, `contribution_${circuit}.zkey`);
+              formData.append('contribution', fs.createReadStream(testPath));
+              formData.append('name', name);
+              formData.append('circuit', circuit);
 
-            axios(config)
-              .then(function (response) {
-                console.log(chalk.green('Contributed to circuit ' + circuit));
-                console.log(chalk.blue('Verification:'));
-                console.log(chalk.blue(response.data.verification));
-                resolve();
-              })
-              .catch(function (error) {
-                console.log(chalk.red(error));
-                reject();
-              });
+              var config = {
+                method: 'post',
+                url: `http://${
+                  branch !== 'main' ? `${branch}.ceremony` : 'ceremony'
+                }.polygon-nightfall.io:3333/upload`,
+                headers: {
+                  ...formData.getHeaders(),
+                },
+                data: formData,
+              };
+
+              axios(config)
+                .then(function (response) {
+                  console.log(chalk.green('Contributed to circuit ' + circuit));
+                  console.log(chalk.blue('Verification:'));
+                  console.log(chalk.blue(response.data.verification));
+                  resolve();
+                })
+                .catch(function (error) {
+                  console.log(chalk.red(error));
+                  reject();
+                });
+            });
           });
-        });
-      }),
-    );
-  });
+        }),
+      );
+    });
 
-  await Promise.all(promises).then(() => {
-    console.log(chalk.bgGreen('Thank you for your contribution!'));
-    process.exit(0);
+    await Promise.all(promises).then(() => {
+      console.log(chalk.bgGreen('Thank you for your contribution!'));
+      process.exit(0);
+    });
   });
-});
 
 program.parse();
