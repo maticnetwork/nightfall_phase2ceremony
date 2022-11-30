177de214bad1060a23115560bb83aec09a0333f2
José Pedro Sousa <github@zepedro.me>
2022-11-30 11:06:21 +0000

    chore: react cleanup

--- browser/src/components/thankYou.js
+++ browser/src/components/thankYou.js
@@ -1,7 +1,7 @@
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import Accordion from '@mui/material/Accordion';
+// import AccordionSummary from '@mui/material/AccordionSummary';
+// import AccordionDetails from '@mui/material/AccordionDetails';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Typography from '@mui/material/Typography';
 
 export default function ThankYou({ circuits, circuitsSubmitted, verifications }) {
